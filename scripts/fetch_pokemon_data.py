423f254dd5dff0879abd9f5d379f1985a8a11c01
Drew Pusey <drewpusey654@gmail.com>
2024-11-05 08:58:18 -0600

    Pokemon types from API

--- scripts/fetch_pokemon_data.py
+++ scripts/fetch_pokemon_data.py
@@ -18,8 +18,21 @@ def get_all_pokemon_species():
         print("Error: ", e)
         return None
 
+def get_all_pokemon():
+    url = "https://pokeapi.co/api/v2/pokemon?limit={}".format(total_pokemon)
+    try:
+        response = requests.get(url)
 
-def get_pokemon_species(url):
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Error: ", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+        return None
+
+def get_data(url):
     try:
         response = requests.get(url)
 
@@ -35,24 +48,37 @@ def get_pokemon_species(url):
 
 def main():
     pokemon_species = get_all_pokemon_species()
+    pokemon = get_all_pokemon()
 
-    if pokemon_species:
+    if pokemon_species and pokemon:
         pokemon_names = []
-        for species in pokemon_species["results"]:
-            species_data = get_pokemon_species(species["url"])
+        for i in range(total_pokemon):
+            species_data = get_data(pokemon_species["results"][i]["url"])
+            pokemon_data = get_data(pokemon["results"][i]["url"])
+
             id = species_data["id"]
             print(f"\rPokemon data fetched: {int(id / total_pokemon * 100)}%", end="", flush=True)
 
             trimmed_data = {}
+            trimmed_data["id"] = id
 
             names = species_data["names"]
             for name in names:
                 if name["language"]["name"] == "en":
-                    trimmed_data["id"] = id
                     trimmed_data["name"] = name["name"]
-                    trimmed_data["color"] = species_data["color"]["name"]
                     pokemon_names.append(name["name"])
                     break
+
+            trimmed_data["types"] = []
+            types = pokemon_data["types"]
+            for type in types:
+                type_data = get_data(type["type"]["url"])
+                type_names = type_data["names"]
+                for type_name in type_names:
+                    if type_name["language"]["name"] == "en":
+                        trimmed_data["types"] += [type_name["name"]]
+
+            trimmed_data["color"] = species_data["color"]["name"]
 
             if not os.path.exists("pokemon"):
                 os.makedirs("pokemon")
