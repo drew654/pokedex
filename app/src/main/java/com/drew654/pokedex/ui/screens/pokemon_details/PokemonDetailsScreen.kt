cdfb11e895b4103cae9d605ac99a20e9d82a9342
Drew Pusey <drewpusey654@gmail.com>
2024-11-06 19:12:53 -0600

    Display pokemon abilities

--- app/src/main/java/com/drew654/pokedex/ui/screens/pokemon_details/PokemonDetailsScreen.kt
+++ app/src/main/java/com/drew654/pokedex/ui/screens/pokemon_details/PokemonDetailsScreen.kt
@@ -2,6 +2,7 @@ package com.drew654.pokedex.ui.screens.pokemon_details
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -37,61 +39,107 @@ fun PokemonDetailsScreen(id: Int) {
     val name = pokemon.jsonObject["name"]?.jsonPrimitive?.content
     val color = pokemon.jsonObject["color"]?.jsonPrimitive?.content
     val types = pokemon.jsonObject["types"]?.jsonArray?.map { it.jsonPrimitive.content }?.toList()
+    val abilities = pokemon.jsonObject["abilities"]?.jsonArray
+    val abilityNames = abilities?.map { ability ->
+        ability.jsonObject["name"]?.jsonPrimitive?.content
+    }
     val backgroundColor = Pokemon().GetBackgroundColor(color.toString())
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+            .background(backgroundColor)
     ) {
-        Row(
-            modifier = Modifier
-                .background(backgroundColor)
-                .padding(8.dp)
-                .fillMaxWidth()
-        ) {
-            Column(
-                modifier = Modifier.weight(1f)
+        Column {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .border(1.dp, Color(0x88000000), RoundedCornerShape(12.dp))
+                    .padding(8.dp)
+                    .fillMaxWidth()
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth()
+                Column(
+                    modifier = Modifier.weight(1f)
                 ) {
-                    Text(
-                        text = name.toString(),
-                        color = Color(0x88000000),
-                        fontSize = 24.sp,
-                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp)
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                    Text(
-                        text = "#${id}",
-                        color = Color(0x88000000),
-                        fontSize = 24.sp,
-                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp)
-                    )
-                }
-                Row {
-                    for (type in types!!) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
                         Text(
-                            text = type.toString(),
+                            text = name.toString(),
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0x88000000),
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp)
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        Text(
+                            text = "#${id}",
                             color = Color(0x88000000),
                             fontSize = 24.sp,
-                            modifier = Modifier
-                                .padding(horizontal = 8.dp, vertical = 8.dp)
-                                .border(1.dp, Color(0x88000000), RoundedCornerShape(12.dp))
-                                .padding(all = 4.dp)
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp)
                         )
                     }
+                    Row {
+                        for (type in types!!) {
+                            Text(
+                                text = type.toString(),
+                                color = Color(0x88000000),
+                                fontSize = 24.sp,
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp, vertical = 8.dp)
+                                    .border(1.dp, Color(0x88000000), RoundedCornerShape(12.dp))
+                                    .padding(all = 4.dp)
+                            )
+                        }
+                    }
+                }
+                AsyncImage(
+                    model = "file:///android_asset/sprites/${id}.png",
+                    contentDescription = name.toString(),
+                    modifier = Modifier
+                        .size(125.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.background)
+                )
+            }
+            Spacer(modifier = Modifier.size(16.dp))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Abilities",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0x88000000)
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .border(1.dp, Color(0x88000000), RoundedCornerShape(12.dp))
+            ) {
+                Column {
+                    for (ability in abilityNames!!) {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                                .border(1.dp, Color(0x88000000), RoundedCornerShape(12.dp))
+                        ) {
+                            Text(
+                                text = ability.toString(),
+                                fontSize = 24.sp,
+                                color = Color(0x88000000),
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(16.dp))
                 }
             }
-            AsyncImage(
-                model = "file:///android_asset/sprites/${id}.png",
-                contentDescription = name.toString(),
-                modifier = Modifier
-                    .size(125.dp)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.background)
-            )
         }
     }
 }
